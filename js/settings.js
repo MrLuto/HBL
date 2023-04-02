43c6dba194d2ad73e46dafa0ad616cffc10eca77
MrLuto <mmudde2@gmail.com>
2023-04-02 15:54:13 +0200

    added logout function

--- js/settings.js
+++ js/settings.js
@@ -16,3 +16,8 @@ cookies_splitted.forEach(cookie => {
 if (loggedIn == false){
     window.location.href = "login.html";
 }
+
+function logout(){
+    document.cookie = "loggedIn=false; expires=Thu, 18 Dec 2028 12:00:00 UTC; path=/;";
+    window.location.href = "login.html";
+}
