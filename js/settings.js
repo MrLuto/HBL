7216064b6b95cd0006c5672be7c6caa3826cfed2
MrLuto <mmudde2@gmail.com>
2023-04-02 16:49:14 +0200

    updated backend connection

--- js/settings.js
+++ js/settings.js
@@ -1,10 +1,4 @@
 let cookies = document.cookie;
-// check if the user is logged in by checking if the cookie is set
-// if the cookie is not set, redirect to login page
-// split cookies into an array then check with an foreach loop if the cookie is set
-// if the cookie is set, set the variable to true
-// if the cookie is not set, set the variable to false
-// if the variable is false, redirect to login page
 let cookies_splitted = cookies.split(";");
 var loggedIn = false;
 cookies_splitted.forEach(cookie => {
