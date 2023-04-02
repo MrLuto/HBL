2b1679dddb50d5ba7338079f4af664bb73204a20
MrLuto <mmudde2@gmail.com>
2023-04-02 18:46:42 +0200

    changed settings menu to become more simplistic

--- js/settings.js
+++ js/settings.js
@@ -1,18 +1,19 @@
 let cookies = document.cookie;
 let cookies_splitted = cookies.split(";");
 var loggedIn = false;
+var email = "";
 cookies_splitted.forEach(cookie => {
     if (cookie == "loggedIn=true"){
         loggedIn = true;
     }else if(cookie.startsWith("user=")){
-        var email = cookie.split("=")[1]
+        email = cookie.split("=")[1];
         document.getElementById("email").value = email;
     }
 });
 
-if (loggedIn == false){
-    window.location.href = "login.html";
-}
+//if (loggedIn == false){
+//    window.location.href = "login.html";
+//}
 
 let url = "http://162.55.245.188:26133/hbl/getinfo?uid=" + email;
 let request = new XMLHttpRequest();
