92248365adb2305d373e26720b4e2e4952bebc99
MrLuto <mmudde2@gmail.com>
2023-04-02 17:12:55 +0200

    get request to get current settings

--- js/settings.js
+++ js/settings.js
@@ -6,12 +6,20 @@ cookies_splitted.forEach(cookie => {
         loggedIn = true;
     }else if(cookie.startsWith("user=")){
         var email = cookie.split("=")[1]
+        document.getElementById("email").value = email;
     }
 });
 
 if (loggedIn == false){
     window.location.href = "login.html";
 }
+
+let url = "http://162.55.245.188:26133/hbl/getinfo?uid=" + email;
+let request = new XMLHttpRequest();
+request.open("GET", url, false);
+request.send(null);
+let response = request.responseText;
+console.log(response);
 
 function logout(){
     document.cookie = "loggedIn=false; expires=Thu, 18 Dec 2028 12:00:00 UTC; path=/;";
