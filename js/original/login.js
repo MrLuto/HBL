09209b9c728a69d27a47e7e0193bab948d530acf
MrLuto <mmudde2@gmail.com>
2023-05-21 15:59:34 +0200

    Optimization | minify js files

---
+++ js/original/login.js
@@ -0,0 +1,36 @@
+window.location.href = "index.html";
+
+let cookies = document.cookie;
+let cookies_splitted = cookies.split(";");
+var loggedIn = false;
+cookies_splitted.forEach(cookie => {
+    if (cookie == "loggedIn=true"){
+        loggedIn = true;
+    }
+});
+
+if (loggedIn == true){
+    window.location.href = "settings.html";
+}
+
+if (window.location.protocol != "http:"){
+    window.location.href = "http://" + window.location.hostname + "/login.html";
+}
+
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let url = "http://162.55.245.188:26133/hbl/login?username=" + username + "&password=" + password;
+    let request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send(null);
+    let response = request.responseText;
+    if (response == "true"){
+        document.cookie = "loggedIn=true; expires=Thu, 18 Dec 2028 12:00:00 UTC; path=/;";
+        document.cookie = "user=" + username + "; expires=Thu, 18 Dec 2028 12:00:00 UTC; path=/;";
+        window.location.href = "settings.html";
+    } else {
+        document.cookie = "loggedIn=false; expires=Thu, 18 Dec 2028 12:00:00 UTC; path=/;";
+        window.location.href = "login.html";
+    }
+}
